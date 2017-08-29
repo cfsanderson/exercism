3553d8af335101f085e9781e52ff63a3160a4284
cfsanderson <calebsanderson@gmail.com>
2017-08-28 21:47:46 -0400

    solved javascript leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,18 +1,23 @@
-//
-// This is only a SKELETON file for the "Leap" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 var Year = function(input) {
-//
-// YOUR CODE GOES HERE
-//  
+  this.year = input;
 };
 
 Year.prototype.isLeap = function() {
-//
-// YOUR CODE GOES HERE
-//
+  if (this.year % 400 === 0) {
+    return true
+  } else if ((this.year % 100 != 0) && (this.year % 4 === 0)) {
+    return true
+  } else {
+    return false
+  }
 };
 
 module.exports = Year;
+
+/*
+isLeap is a year that is
+year%4 === 0 but NOT
+year%100 === 0 UNLESS
+year%400 === 0
+
+*/
