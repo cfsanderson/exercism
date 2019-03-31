a3c45054963632f26c16305fc2f4caf1592d6b05
cfsanderson <calebsanderson@gmail.com>
2019-03-31 06:05:57 -0400

    updating

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -1,13 +1,13 @@
 const Year = input => this.year = input;
 
-// Year.prototype.isLeap = function() {
-//   if (this.year % 400 === 0) {
-//     return true
-//   } else if ((this.year % 100 != 0) && (this.year % 4 === 0)) {
-//     return true
-//   } else {
-//     return false
-//   }
-// };
-//
-// module.exports = Year;
+Year.prototype.isLeap = function() {
+  if (this.year % 400 === 0) {
+    return true
+  } else if ((this.year % 100 != 0) && (this.year % 4 === 0)) {
+    return true
+  } else {
+    return false
+  }
+};
+
+module.exports = Year;
